361feea4e86e72a03d0222ee3b1bcc1c643ab97b
Frederico Espeschit <fespeschitt@gmail.com>
2021-02-15 00:23:50 -0300

    Primeiros passos com javascript

--- script.js
+++ script.js
@@ -60,13 +60,32 @@
 
 //Abre e fecha a lateral
 
-function menu() {
-    var controle = document.getElementById("menu-opener").innerHTML = ("width:200px;");
-    if (controle == 200) {
-        document.getElementById("menu-opener").innerHTML = ("width:0px;");
+// function menu() {
+//     var controle = document.getElementById("menu-opener").innerHTML = ("width:200px;");
+//     if (controle == 200) {
+//         document.getElementById("menu-opener").innerHTML = ("width:0px;");
 
-    } else {
-        document.getElementById("menu-opener").innerHTML = ("width:200px;");
-    }
+//     } else {
+//         document.getElementById("menu-opener").innerHTML = ("width:200px;");
+//     }
     
+// }
+
+function azul(){
+    limpar();
+    document.getElementById("titulo").classList.add("azul");
+}
+function vermelho(){
+    limpar();
+    document.getElementById("titulo").classList.add("vermelho");
+}
+function verde(){
+    limpar();
+    document.getElementById("titulo").classList.add("verde");
+}
+
+function limpar(){
+    document.getElementById("titulo").classList.remove("azul");
+    document.getElementById("titulo").classList.remove("vermelho");
+    document.getElementById("titulo").classList.remove("verde");
 }
